6892a27c8ebc063879ec99aa0fd80d6adff7ebc9
Isaac Santos <isaacdsantos@hotmail.com>
2023-04-20 16:01:03 -0300

    Index atualizado com ação necessária com requisitos

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ programa
     .description('Gerador de senhas entre 12 e 24 caracteres');
 
 programa
-  .option('-t, --tamanho <number>', 'define um tamanho para a senha (padrao 12)', parseInt);
+  .option('-t, --tamanho <number>', 'define um tamanho para a senha (padrao 12)', parseInt)
+  .action(()=>{
+        let tamanhoSenha = programa.opts().tamanho || 12;
+
+        if (tamanhoSenha < 12) {
+            tamanhoSenha = 12;
+        } else if (tamanhoSenha > 24) {
+            tamanhoSenha = 24;
+        }
+
+        let senha = geradorSenha(tamanhoSenha);
+        console.log(tamanhoSenha);
+    });
   
 
 programa.parse(process.argv);
