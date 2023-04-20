b08b17a2b74641be77c22f3d0fe2e0e3112191a7
Isaac Santos <isaacdsantos@hotmail.com>
2023-04-20 15:34:26 -0300

    atualizado arquivo index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const programa = new Command();
 
 programa
     .version('1.0.0')
-    .description('Gerador de senhas');
+    .description('Gerador de senhas entre 12 e 24 caracteres');
 
 programa
   .option('-t, --tamanho <number>', 'define um tamanho para a senha (padrao 12)', parseInt);
